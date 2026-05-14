e611b434f44584f0a4e91f21161579e90e17f36b
Ryan Russo <ryangoreckirusso12@gmail.com>
2026-05-13 21:35:49 -0300

    feat: banco e recursos em javascript (incompleto)

--- SAA/scripts/scripts_login.js
+++ SAA/scripts/scripts_login.js
@@ -1,3 +1,4 @@
+// Máscara do cpf
 const inputCPF = document.getElementById("cpf")
 
 inputCPF.addEventListener("input", (e) =>{
@@ -11,3 +12,28 @@ inputCPF.addEventListener("input", (e) =>{
 
     e.target.value = valor;
 });
+
+// Funcionalidade de validação do login
+async function login(event) {
+    event.preventDefault();
+
+    let cpf = document.getElementById('cpf').value.replace(/\D/g, "")
+    let senha = document.getElementById('senha').value
+
+    try {
+        const resposta = await fetch("scripts/usuarios.json")
+        const usuarios = await resposta.json();
+
+        const usuario = usuarios.find(u =>
+            u.cpf === cpf && u.senha === senha
+        );
+        
+        if (usuario) {
+            if (usuario.tipo === "aluno") {
+                window.location.href = "tela_aluno.html"
+            };
+        };
+    } catch {
+        alert('Erro!')
+    }
+}
